a63ddcb38320711ed25ac9f8cb6c9c969c827827
abhinav4067 <abhinavb4067@gmail.com>
2026-01-21 16:17:53 +0530

    bug fixing

--- src/pages/admin/SupportAdminMyTicketsPage.jsx
+++ src/pages/admin/SupportAdminMyTicketsPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../../components/common/Sidebar";
 import Header from "../../components/common/Header";
-import "../../assets/styles/user.css";
+import "../../assets/styles/admin.css";
 import {
   listSuperAdminTickets,
   filterSuperAdminTickets,
